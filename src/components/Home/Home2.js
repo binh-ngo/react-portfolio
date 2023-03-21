1ee6eab8b0b8bf315aceec8058fd7747db2afca7
Binh Ngo <bnngo97@gmail.com>
2023-03-21 10:59:10 -0700

    updated img extensions

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {AiFillGithub} from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -49,9 +48,7 @@ function Home2() {
             </p>
           </Col>
           <Col md={4} className="myAvtar">
-            <Tilt>
-              {/* <img src={myImg} className="img-fluid" alt="avatar" /> */}
-            </Tilt>
+
           </Col>
         </Row>
         <Row>
