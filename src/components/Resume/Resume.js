7b33f1758997c01938faa95e63b8e848157ba275
Binh Ngo <bnngo97@gmail.com>
2023-03-30 10:18:12 -0700

    fixing resume link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../assets/codingresume-3-30.pdf";
+import pdf from "../../assets/coding-resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
