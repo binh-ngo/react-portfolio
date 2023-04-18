b2df03aac65fdf5139de043a8cfbff94dfcffb81
Binh Ngo <bnngo97@gmail.com>
2023-04-18 10:31:42 -0700

    fixed laptop image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ function About() {
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
-            <img src={laptop} alt="about" className="blob" />
+            <img src={laptop} alt="about" className="blob-about" />
           </Col>
         </Row>
         <h1 className="project-heading">
